4f8c1421e545363e06740da9b9e079537d5e152f
songwritterfx <songwritterfx@gmail.com>
2014-03-23 12:27:59 +0530

    ass

--- js/bootstrap-demo.js
+++ js/bootstrap-demo.js
@@ -1,5 +1,20 @@
+
 $('.redirect').click( function() {
-
-	$('header')
-
+	$('#toAnimate').animate({
+		margin:'auto auto auto -1000px'
+		}, 300, function(){													//================ 1st Callback 
+			$('.redirect').animate({opacity:0},500,
+				function(){													//================ 2nd Callback
+					$('section').slideUp(
+						function(){				
+							$('#favico').toggleClass("pull-right");
+							$('#favico').css("width","50px");
+							$('#favico').animate({margin:'-220px auto auto 400px'},
+								function() {								//================ 3rd callback
+									$('body').css('background-color','white');
+								}
+							);
+						});
+				});	
+		});
 });
